cbf1aa1d78914e0e265f1293c6ecf73de2f83b49
RodrigoDornelles <rodrigo@dornelles.me>
2020-11-09 10:26:50 -0300

    add undefine notes header

---
+++ include/ToneUndef.h
@@ -0,0 +1,291 @@
+#ifdef NOTE_B0
+#undef NOTE_B0
+#endif
+#ifdef NOTE_C1
+#undef NOTE_C1
+#endif
+#ifdef NOTE_CS1
+#undef NOTE_CS1
+#endif
+#ifdef NOTE_D1
+#undef NOTE_D1
+#endif
+#ifdef NOTE_DS1
+#undef NOTE_DS1
+#endif
+#ifdef NOTE_E1
+#undef NOTE_E1
+#endif
+#ifdef NOTE_F1
+#undef NOTE_F1
+#endif
+#ifdef NOTE_FS1
+#undef NOTE_FS1
+#endif
+#ifdef NOTE_G1
+#undef NOTE_G1
+#endif
+#ifdef NOTE_GS1
+#undef NOTE_GS1
+#endif
+#ifdef NOTE_A1
+#undef NOTE_A1
+#endif
+#ifdef NOTE_AS1
+#undef NOTE_AS1
+#endif
+#ifdef NOTE_B1
+#undef NOTE_B1
+#endif
+#ifdef NOTE_C2
+#undef NOTE_C2
+#endif
+#ifdef NOTE_CS2
+#undef NOTE_CS2
+#endif
+#ifdef NOTE_D2
+#undef NOTE_D2
+#endif
+#ifdef NOTE_DS2
+#undef NOTE_DS2
+#endif
+#ifdef NOTE_E2
+#undef NOTE_E2
+#endif
+#ifdef NOTE_F2
+#undef NOTE_F2
+#endif
+#ifdef NOTE_FS2
+#undef NOTE_FS2
+#endif
+#ifdef NOTE_G2
+#undef NOTE_G2
+#endif
+#ifdef NOTE_GS2
+#undef NOTE_GS2
+#endif
+#ifdef NOTE_A2
+#undef NOTE_A2
+#endif
+#ifdef NOTE_AS2
+#undef NOTE_AS2
+#endif
+#ifdef NOTE_B2
+#undef NOTE_B2
+#endif
+#ifdef NOTE_C3
+#undef NOTE_C3
+#endif
+#ifdef NOTE_CS3
+#undef NOTE_CS3
+#endif
+#ifdef NOTE_D3
+#undef NOTE_D3
+#endif
+#ifdef NOTE_DS3
+#undef NOTE_DS3
+#endif
+#ifdef NOTE_E3
+#undef NOTE_E3
+#endif
+#ifdef NOTE_F3
+#undef NOTE_F3
+#endif
+#ifdef NOTE_FS3
+#undef NOTE_FS3
+#endif
+#ifdef NOTE_G3
+#undef NOTE_G3
+#endif
+#ifdef NOTE_GS3
+#undef NOTE_GS3
+#endif
+#ifdef NOTE_A3
+#undef NOTE_A3
+#endif
+#ifdef NOTE_AS3
+#undef NOTE_AS3
+#endif
+#ifdef NOTE_B3
+#undef NOTE_B3
+#endif
+#ifdef NOTE_C4
+#undef NOTE_C4
+#endif
+#ifdef NOTE_CS4
+#undef NOTE_CS4
+#endif
+#ifdef NOTE_D4
+#undef NOTE_D4
+#endif
+#ifdef NOTE_DS4
+#undef NOTE_DS4
+#endif
+#ifdef NOTE_E4
+#undef NOTE_E4
+#endif
+#ifdef NOTE_F4
+#undef NOTE_F4
+#endif
+#ifdef NOTE_FS4
+#undef NOTE_FS4
+#endif
+#ifdef NOTE_G4
+#undef NOTE_G4
+#endif
+#ifdef NOTE_GS4
+#undef NOTE_GS4
+#endif
+#ifdef NOTE_A4
+#undef NOTE_A4
+#endif
+#ifdef NOTE_AS4
+#undef NOTE_AS4
+#endif
+#ifdef NOTE_B4
+#undef NOTE_B4
+#endif
+#ifdef NOTE_C5
+#undef NOTE_C5
+#endif
+#ifdef NOTE_CS5
+#undef NOTE_CS5
+#endif
+#ifdef NOTE_D5
+#undef NOTE_D5
+#endif
+#ifdef NOTE_DS5
+#undef NOTE_DS5
+#endif
+#ifdef NOTE_E5
+#undef NOTE_E5
+#endif
+#ifdef NOTE_F5
+#undef NOTE_F5
+#endif
+#ifdef NOTE_FS5
+#undef NOTE_FS5
+#endif
+#ifdef NOTE_G5
+#undef NOTE_G5
+#endif
+#ifdef NOTE_GS5
+#undef NOTE_GS5
+#endif
+#ifdef NOTE_A5
+#undef NOTE_A5
+#endif
+#ifdef NOTE_AS5
+#undef NOTE_AS5
+#endif
+#ifdef NOTE_B5
+#undef NOTE_B5
+#endif
+#ifdef NOTE_C6
+#undef NOTE_C6
+#endif
+#ifdef NOTE_CS6
+#undef NOTE_CS6
+#endif
+#ifdef NOTE_D6
+#undef NOTE_D6
+#endif
+#ifdef NOTE_DS6
+#undef NOTE_DS6
+#endif
+#ifdef NOTE_E6
+#undef NOTE_E6
+#endif
+#ifdef NOTE_F6
+#undef NOTE_F6
+#endif
+#ifdef NOTE_FS6
+#undef NOTE_FS6
+#endif
+#ifdef NOTE_G6
+#undef NOTE_G6
+#endif
+#ifdef NOTE_GS6
+#undef NOTE_GS6
+#endif
+#ifdef NOTE_A6
+#undef NOTE_A6
+#endif
+#ifdef NOTE_AS6
+#undef NOTE_AS6
+#endif
+#ifdef NOTE_B6
+#undef NOTE_B6
+#endif
+#ifdef NOTE_C7
+#undef NOTE_C7
+#endif
+#ifdef NOTE_CS7
+#undef NOTE_CS7
+#endif
+#ifdef NOTE_D7
+#undef NOTE_D7
+#endif
+#ifdef NOTE_DS7
+#undef NOTE_DS7
+#endif
+#ifdef NOTE_E7
+#undef NOTE_E7
+#endif
+#ifdef NOTE_F7
+#undef NOTE_F7
+#endif
+#ifdef NOTE_FS7
+#undef NOTE_FS7
+#endif
+#ifdef NOTE_G7
+#undef NOTE_G7
+#endif
+#ifdef NOTE_GS7
+#undef NOTE_GS7
+#endif
+#ifdef NOTE_A7
+#undef NOTE_A7
+#endif
+#ifdef NOTE_AS7
+#undef NOTE_AS7
+#endif
+#ifdef NOTE_B7
+#undef NOTE_B7
+#endif
+#ifdef NOTE_C8
+#undef NOTE_C8
+#endif
+#ifdef NOTE_CS8
+#undef NOTE_CS8
+#endif
+#ifdef NOTE_D8
+#undef NOTE_D8
+#endif
+#ifdef NOTE_DS8
+#undef NOTE_DS8
+#endif
+#ifdef NOTE_E8
+#undef NOTE_E8
+#endif
+#ifdef NOTE_F8
+#undef NOTE_F8
+#endif
+#ifdef NOTE_FS8
+#undef NOTE_FS8
+#endif
+#ifdef NOTE_G8
+#undef NOTE_G8
+#endif
+#ifdef NOTE_GS8
+#undef NOTE_GS8
+#endif
+#ifdef NOTE_A8
+#undef NOTE_A8
+#endif
+#ifdef NOTE_AS7
+#undef NOTE_AS7
+#endif
+#ifdef NOTE_B8
+#undef NOTE_B8
+#endif
